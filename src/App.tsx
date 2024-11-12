e771398369ffe1c59d5be7328aef04f81dd53b05
EriveltonMGit <eriveltonprojetos.git@gmail.com>
2024-11-12 15:10:10 -0300

    chore: incluindo pagina 5 vazia

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Page_1 from "./Pages/Page_1";
 import Page_2 from "./Pages/Page_2";
 import  Page_3  from "./Pages/Page_3";
 import  Page_4  from "./Pages/Page_4";
+import  Page_5  from "./Pages/Page_5";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
       <Page_2 />
       <Page_3/>
       <Page_4 />
+      <Page_5 />
     </section>
   );
 }
---
+++ src/Pages/Page_5/index.tsx
@@ -0,0 +1,4 @@
+import "./Page_5.style.css";
+export default function Page_5() {
+  return <section className="section_5">PÁGINA 5</section>;
+}
